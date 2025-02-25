2df800789497eae8499a0eabcdf69b15fcb11e92
Adam M <adamd10bv@gmail.com>
2025-02-24 23:42:49 -0500

    fix can ids

--- src/main/java/frc/robot/HardwareIds.java
+++ src/main/java/frc/robot/HardwareIds.java
@@ -11,21 +11,21 @@ public class HardwareIds {
      */
     public static class Can {
         //TODO these are reasonable defaults, update to reflect the robot
-        public static final int FRONT_LEFT_DRIVE_MOTOR = 0;
-        public static final int FRONT_LEFT_STEER_MOTOR = 4;
-        public static final int FRONT_LEFT_ENCODER = 8;
+        public static final int FRONT_LEFT_DRIVE_MOTOR = 1;
+        public static final int FRONT_LEFT_STEER_MOTOR = 5;
+        public static final int FRONT_LEFT_ENCODER = 9;
 
-        public static final int FRONT_RIGHT_DRIVE_MOTOR = 1;
-        public static final int FRONT_RIGHT_STEER_MOTOR = 5;
-        public static final int FRONT_RIGHT_ENCODER = 9;
+        public static final int FRONT_RIGHT_DRIVE_MOTOR = 2;
+        public static final int FRONT_RIGHT_STEER_MOTOR = 6;
+        public static final int FRONT_RIGHT_ENCODER = 10;
 
-        public static final int BACK_RIGHT_DRIVE_MOTOR = 2;
-        public static final int BACK_RIGHT_STEER_MOTOR = 6;
-        public static final int BACK_RIGHT_ENCODER = 10;
+        public static final int BACK_RIGHT_DRIVE_MOTOR = 3;
+        public static final int BACK_RIGHT_STEER_MOTOR = 7;
+        public static final int BACK_RIGHT_ENCODER = 11;
 
-        public static final int BACK_LEFT_DRIVE_MOTOR = 3;
-        public static final int BACK_LEFT_STEER_MOTOR = 7;
-        public static final int BACK_LEFT_ENCODER = 11;
+        public static final int BACK_LEFT_DRIVE_MOTOR = 4;
+        public static final int BACK_LEFT_STEER_MOTOR = 8;
+        public static final int BACK_LEFT_ENCODER = 12;
 
         public static final int IMU = 13; //gyro
 
