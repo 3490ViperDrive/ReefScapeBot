d85179f2e1c109284fbd27aa15fa57b86d7e7f81
Lhaj3 <54109759+Lhaj3@users.noreply.github.com>
2025-03-01 13:29:55 -0500

    Elevator (#5)
    * Added elevator subsystem
    
    * changed things
    
    * added setPosition method to subsystem
    
    * SetElevator function is finished
    
    * fixed SetElevator command + subsystem function
    
    * replaced hashmap with enum
    
    * small changes
    
    ---------
    
    Co-authored-by: Coltonfr5 <Coltonfr567@gmail.com>

---
+++ src/main/java/frc/robot/commands/SetElevator.java
@@ -0,0 +1,20 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Elevator.Positions;
+
+public class SetElevator extends Command {
+
+    Positions position;
+    Elevator elevator;
+    public SetElevator(Positions _position, Elevator _elevator){
+        position = _position;
+        elevator = _elevator;
+        addRequirements(elevator);
+    }
+    @Override
+    public void execute(){
+        elevator.setElevator(position.get());
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.DeviceIdentifier;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase{
+
+    TalonFX motorController;
+    public enum Positions{
+        // TODO: set values 
+        DEFAULT(0.0),
+        ALGAE_L2(0.0),
+        ALGAE_L3(0.0),
+        CORAL_L1(0.0),
+        CORAL_L2(0.0),
+        CORAL_L3(0.0),
+        CORAL_L4(0.0);
+
+        double position;
+
+        Positions(double position){
+            this.position = position;
+        }
+
+        public double get(){
+            return this.position;
+        }
+    }
+
+    public Elevator(){
+        this.setDefaultCommand(null);
+
+    }
+
+    public void setElevator(double rawPosition){
+        // TODO: do something to the position
+        motorController.setControl(new PositionVoltage(rawPosition));
+    }
+
+    public void configureMotor() {
+        motorController = new TalonFX(0);
+        TalonFXConfigurator configurator = new TalonFXConfigurator(new DeviceIdentifier(motorController.getDeviceID(), getSubsystem(), getName()));
+    }
+}
