38adb5c951c3a229982bf071977db029b47ae68f
Adam M <adamd10bv@gmail.com>
2025-03-05 17:03:46 -0500

    Robot.java can have a little more logging as a treat

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,8 +13,10 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 @Logged
 public class Robot extends TimedRobot {
@@ -57,6 +59,10 @@ public class Robot extends TimedRobot {
     });
     Epilogue.bind(this);
     DataLogManager.start();
+    //log joystick data
+    DriverStation.startDataLog(DataLogManager.getLog());
+    //logging fr
+    SmartDashboard.putData(CommandScheduler.getInstance());
   }
 
   @Override
